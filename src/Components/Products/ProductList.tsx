52016098c64b60946665813b3a27de96e5c5a4fe
Sadat Hillary Kollan <sadat@farmerline.co>
2023-02-19 23:40:08 +0000

    additions

--- src/Components/Products/ProductList.tsx
+++ src/Components/Products/ProductList.tsx
@@ -66,7 +66,7 @@ export default function () {
     setIsLoading(false);
     setError(false);
      //Name
-     if (!itemNumber) {
+     if (!itemNumber && !type) {
       console.log("here");
       setError(true);
       setErrorValue("Item number cannot be empty");
